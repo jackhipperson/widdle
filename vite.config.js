6a5fb90c00519fb83bef722e69a42003963c3653
Jack Hipperson <jackhipperson@hotmail.com>
2023-09-01 15:57:44 +0100

    dev

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    base: '/widdle',
+    base: '/widdle/',
     plugins: [react()]
-  }
-
-  if (command !== 'serve') {
-    config.base = '/widdle/'
   }
 
   return config
